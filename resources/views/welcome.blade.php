c18444af2c57526902485ad1f4f74db609b3f0dc
Salim Djerbouh <caddydz4@gmail.com>
2019-09-10 23:18:40 +0100

    Work In Progress
    This is a trash commit to save changes
    
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,10 @@
+@verbatim
+<!-- <img src="{{ asset('exurb1a.png') }}" alt="Image not found"> -->
+@endverbatim
+<style>
+    * {
+        width: 99%;
+        height: 99%;
+    }
+</style>
 <img src="{{ asset('exurb1a.png') }}" alt="Image not found">
